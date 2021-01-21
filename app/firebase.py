68eb76ce9d5a1f609c998568f1989f50eef0c4d3
devdattakhoche <dkhoche70@gmail.com>
2021-01-21 16:44:21 +0530

    🎨  Completed patient details

--- app/firebase.py
+++ app/firebase.py
@@ -62,5 +62,24 @@ def get_blacklist():
 			traceback.print_exc()
 		return False		
 
-get_blacklist()
 
+def Check_breach(uid):
+	"""
+	Returns boolean value if the patient breached or not in the database
+	"""
+	try:
+		doc_ref = db.collection(u'blacklist')
+		snapshots = list(doc_ref.where(u'patientId', u'==', uid).stream())
+		if snapshots[0] is None:
+			return False
+		else:
+			return snapshots[0].to_dict()
+	except IndexError :
+		return False
+	except Exception as ex:
+		print('Exception Occurred which is of type :', ex.__class__.__name__)
+		y = input('If you want to see Traceback press 1 : ')
+		if y == '1':
+			traceback.print_exc()
+		return False
+	
